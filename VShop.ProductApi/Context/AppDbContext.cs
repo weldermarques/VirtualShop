7032d1ba5c3b118143618b3bf4be83a35c580104
welde <welde@WELDER>
2025-04-05 18:25:28 -0300

    Add project files.

---
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
